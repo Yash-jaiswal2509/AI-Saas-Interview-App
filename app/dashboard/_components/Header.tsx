c00a899cce5179652558a28a7f11ae9da52d6d52
Yash-jaiswal2509 <yashjaiswal2509@gmail.com>
2024-06-20 23:57:46 +0530

    (server): drizzle orm setup

--- app/dashboard/_components/Header.tsx
+++ app/dashboard/_components/Header.tsx
@@ -1,17 +1,61 @@
+"use client";
+
+import { cn } from "@/lib/utils";
 import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
-import React from "react";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
+  const pathname = usePathname();
+
   return (
     <div className="flex p-4 justify-between items-center border-b shadow-sm">
-      <Image src="/Website Logo.png" alt="logo" width={100} height={50} className="h-14 w-14 rounded-full" />
-      <ul className="flex gap-5">
-        <li className="hover:text-primary hover:font-bold  transition-all cursor-pointer">Dashboard</li>
-        <li className="hover:text-primary hover:font-bold  transition-all cursor-pointer">Questions</li>
-        <li className="hover:text-primary hover:font-bold  transition-all cursor-pointer">Upgrade</li>
-        <li className="hover:text-primary hover:font-bold  transition-all cursor-pointer">How It Works?</li>
+      <div className="flex gap-2 items-center">
+        <Image
+          src="/Website Logo.png"
+          alt="logo"
+          width={100}
+          height={50}
+          className="h-14 w-14 rounded-full"
+        />
+        <h1 className="text-2xl font-extrabold text-primary">InterviewGenie</h1>
+      </div>
+
+      <ul className="gap-5 hidden md:flex">
+        <li
+          className={cn(
+            "hover:text-primary hover:font-bold  transition-all cursor-pointer",
+            pathname === "/dashboard" ? "text-primary font-bold" : ""
+          )}
+        >
+          Dashboard
+        </li>
+        <li
+          className={cn(
+            "hover:text-primary hover:font-bold  transition-all cursor-pointer",
+            pathname === "/questions" ? "text-primary font-bold" : ""
+          )}
+        >
+          Questions
+        </li>
+        <li
+          className={cn(
+            "hover:text-primary hover:font-bold  transition-all cursor-pointer",
+            pathname === "/upgrade" ? "text-primary font-bold" : ""
+          )}
+        >
+          Upgrade
+        </li>
+        <li
+          className={cn(
+            "hover:text-primary hover:font-bold  transition-all cursor-pointer",
+            pathname === "/how-it-works" ? "text-primary font-bold" : ""
+          )}
+        >
+          How It Works?
+        </li>
       </ul>
+
       <UserButton />
     </div>
   );
---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  schema: "./utils/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.NEXT_PUBLIC_DRIZZLE_DB_URL! as string,
+  },
+  verbose: true,
+  strict: true,
+});
